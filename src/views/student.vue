6f00d826e30c51800720494e7884b7015b9de74b
mary <thoeun.mary@institute.pse.ngo>
2025-06-12 14:10:31 +0700

    add featute student list

--- src/views/student.vue
+++ src/views/student.vue
@@ -64,7 +64,6 @@
             </tr>
           </thead>
           <tbody>
-            <!-- Example Row -->
             <tr
               class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
             >
