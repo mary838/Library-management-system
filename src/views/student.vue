78290b722f0d3cdddb1cd0ac8b66b0193fb9afc8
mary <thoeun.mary@institute.pse.ngo>
2025-06-12 16:10:05 +0700

    edit conflic

--- src/views/student.vue
+++ src/views/student.vue
@@ -68,23 +68,10 @@
             :key="i"
             class="bg-white border-b border-gray-200 hover:bg-gray-50"
           >
-<<<<<<< HEAD
-            <tr>
-              <th scope="col" class="px-6 py-3 text-blue-600">Book Name</th>
-              <th scope="col" class="px-6 py-3 text-blue-600">ID Book</th>
-              <th scope="col" class="px-6 py-3 text-blue-600">Type</th>
-              <th scope="col" class="px-6 py-3 text-blue-600">Timeline</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr
-              class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
-=======
             <td class="w-4 p-4"></td>
             <th
               scope="row"
               class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
->>>>>>> origin/main
             >
               Student {{ i }}
             </th>
